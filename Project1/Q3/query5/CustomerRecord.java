840b29996efb35f7099369032efd92fbcfa39cef
wenleicao <wenleicao@hotmail.com>
2017-02-12 07:53:17 -0500

    add query5 and modified project doc

---
+++ Project1/Q3/query5/CustomerRecord.java
@@ -0,0 +1,43 @@
+package query5;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+
+public class CustomerRecord implements Writable {
+
+    public Text name = new Text();
+    public FloatWritable salary = new FloatWritable();
+
+    public CustomerRecord(){}
+               
+    public CustomerRecord(String Name, float Salary){
+        this.name.set(Name);
+        this.salary.set(Salary);
+    }
+    
+    public CustomerRecord(String Name){
+        this.name.set(Name);
+       
+    }
+
+      
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		 this.name.readFields(in);
+	        this.salary.readFields(in);
+		
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		 this.name.write(out);
+	        this.salary.write(out);
+		
+	}
+}
