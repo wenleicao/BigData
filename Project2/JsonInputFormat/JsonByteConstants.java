7abf7d6c5473dc403cc1f7eb8b67d013049bb142
wenleicao <wenleicao@hotmail.com>
2017-02-25 12:05:44 -0500

    add jsoninput

---
+++ Project2/JsonInputFormat/JsonByteConstants.java
@@ -0,0 +1,19 @@
+package Q2;
+
+public class JsonByteConstants {
+	
+	public static final int JSON_START_BRACE = 123;
+	
+	public static final int JSON_END_BRACE = 125;
+	
+	public static final int JSON_COLON = 58;
+	
+	public static final int JSON_COMMA = 44;
+	
+	public static final int JSON_ARRAY_START = 91;
+	
+	public static final int JSON_ARRAY_END = 93;
+	
+	public static final int JSON_QUOTE = 34;
+
+}
