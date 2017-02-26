11ae9649f9883de14a121b84ef1723e8ab70dd99
caitlinkuhlman <caitlinkuhlman@gmail.com>
2017-02-26 12:05:05 -0500

    data generator for spatial join

--- Project2/sjoin/DataGenerator.java
+++ Project2/sjoin/DataGenerator.java
@@ -1,4 +1,4 @@
-package Q1;
+package sjoin;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -8,96 +8,84 @@ import java.util.Random;
 import org.apache.commons.lang.RandomStringUtils;
 
 /**
- * Program that create two datasets (two comma separated files), Customers and Transactions.
+ * Program that create two datasets (two comma separated files), Customers and
+ * Transactions.
+ * 
  * @author caitlin
  *
  */
 public class DataGenerator {
-	
+
 	static Random random = new Random();
-	
-	
+
 	public static void main(String[] args) {
-		
-//		The	Customers dataset:
-//		ID:	unique	sequential	number	(integer)	from	1	to	50,000	(that	is	the	file	will	have	50,000	line)
-//		Name:	random	sequence	of	characters	of	length	between	10	and	20	(do	not	include	commas)
-//		Age:	random	number	(integer)	between	10	to	70
-//		CountryCode:	random	number	(integer)	between	1	and	10
-//		Salary:	random	number	(float)	between	100	and	10000
+
+		/**
+		 * Generator for spatial join data.
+		 * Generates two sets of data: P random points in space and R random rectangles in space.
+		 * Space assumed to be 2d from 1 to 10000 along each axis
+		 */
 
 		BufferedWriter bw = null;
 		try {
-			
-			int id;
-			int age;
-			int countryCode;
-			String name;
-			float salary;
-			
-			bw = new BufferedWriter(new FileWriter("customers.csv"));
-			for(id=1 ; id<50001 ; id++){
-				age = random.nextInt(61) + 10;
-				countryCode = random.nextInt(10) + 1;
-				name = RandomStringUtils.randomAlphabetic(random.nextInt(11)+10);
-				salary = (random.nextFloat()*9900)+100;
-				bw.write(id+","+name+","+age+","+countryCode+","+salary+"\n");
-			}
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		finally{
-			try{
-				if (bw != null){
-				bw.close();
-				}
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
-		}
-		
-//		The	Transactions dataset:
-//		TransID:	unique	sequential	number	(integer)	from	1	to	5,000,000	(the	file	has 5M	transactions)
-//		CustID:	References	one	of	the customer	IDs,	i.e.,	from	1	to	50,000 (on	Avg.	a customer	has	100 trans.)
-//		TransTotal:	random	number	(float)	between	10	and	1000
-//		TransNumItems:	random	number	(integer)	between	1	and	10
-//		TransDesc:	random	text	of	characters	of	length	between	20	and	50	(do	not	include	commas)
-		
-		try  {
-
+			int x;
+			int y;
 			int id;
-			int custID;
-			float transTotal;
-			int transNumItems;
-			String transDesc;
-			
-			bw = new BufferedWriter(new FileWriter("transactions.csv"));
-			for(id=1 ; id<5000001 ; id++){
-				custID = random.nextInt(50000) + 1;
-				transTotal = (random.nextFloat()*990)+10;
-				transNumItems = random.nextInt(10) + 1;
-				transDesc = RandomStringUtils.randomAlphabetic(random.nextInt(31)+20);
+
+			bw = new BufferedWriter(new FileWriter("points.csv"));
+			for (id = 1; id < 12000000; id++) {
 				
-				bw.write(id+","+custID+","+transTotal+","+transNumItems+","+transDesc+"\n");
+				x = random.nextInt(10000) + 1;
+				y = random.nextInt(10000) + 1;
+				bw.write(x + "," + y + "\n");
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally{
-			try{
-				if (bw != null){
-				bw.close();
+		} finally {
+			try {
+				if (bw != null) {
+					bw.close();
 				}
-			}
-			catch(IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+
+		try {
+
+			int x;
+			int y;
+			int h;
+			int w;
+			int id;
+
+			bw = new BufferedWriter(new FileWriter("rectangles.csv"));
+			for (id = 1; id < 2500000; id++) {
+
+				x = random.nextInt(10000) + 1;
+				y = random.nextInt(10000) + 1;
+				h = random.nextInt(20) + 1;
+				w = random.nextInt(5) + 1;
+				
+//				format: x,y,x+w,y,x,y+h,x+w,y+h 
+				bw.write(x +","+ y +","+ x+w +","+ y +","+ x +","+ y+h +","+ x+w +","+ y+h +"\n");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		System.out.println("Done");
 	}
 
 }
-
