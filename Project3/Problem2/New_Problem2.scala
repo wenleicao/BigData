9f8f4a0dbedc99b5a0a8a064573a6d3cd134382e
caitlinkuhlman <caitlinkuhlman@gmail.com>
2017-03-21 23:30:07 -0400

    second part of question 2 complete - really

--- Project3/Problem2/New_Problem2.scala
+++ Project3/Problem2/New_Problem2.scala
@@ -8,39 +8,47 @@ import java.lang.Math
 //define helper functions
 object Functions{
 
+//function to map point to cell
     def findCell(p:Array[Int]): Int = {
         Math.floor((p.apply(1)-10000)*(-1)/20).toInt * 500 + Math.floor(p.apply(0)/20+1).toInt
     }
 
+//find neighbors of corner cell
     def findCorner(id:Int, count:Float): Array[(Int,String)] = {
     id match {
-	    case 1 => Array((id,count+"|c"),(id+1,count+"|n"),  (id+500,count+"|n"), (id+501,count+"|n"))
-	    case 500 => Array((id,count+"|c"), (id-1,count+"|n"), (id+500,count+"|n"),(id+499,count+"|n"))
-	    case 249501 => Array((id,count+"|c"),(id+1,count+"|n"), (id-500,count+"|n"),(id-499,count+"|n"))
-	    case 250000 => Array((id,count+"|c"), (id-1,count+"|n"), (id-500,count+"|n"), (id-501,count+"|n"))
+	    case 1 => Array((id,count+"|c"),(id+1,count+"|n|"+id),  (id+500,count+"|n|"+id), (id+501,count+"|n|"+id))
+	    case 500 => Array((id,count+"|c"), (id-1,count+"|n|"+id), (id+500,count+"|n|"+id),(id+499,count+"|n|"+id))
+	    case 249501 => Array((id,count+"|c"),(id+1,count+"|n|"+id), (id-500,count+"|n|"+id),(id-499,count+"|n|"+id))
+	    case 250000 => Array((id,count+"|c"), (id-1,count+"|n|"+id), (id-500,count+"|n|"+id), (id-501,count+"|n|"+id))
 	}
     }
 
     def findTop(id:Int, count:Float): Array[(Int,String)] = {
 	//println("top")
-	Array((id,count+"|c"),(id+1,count+"|n"), (id-1,count+"|n"), (id+500,count+"|n"),  (id+501,count+"|n"), (id+499,count+"|n"))
+	Array((id,count+"|c"),(id+1,count+"|n|"+id), (id-1,count+"|n|"+id), (id+500,count+"|n|"+id),  (id+501,count+"|n|"+id), (id+499,count+"|n|"+id))
     }
 
+//find neighbors of cell in bottom row
     def findBottom(id:Int, count:Float): Array[(Int,String)] = {
 	//println("bottom")
-	Array((id,count+"|c"),(id+1,count+"|n"), (id-1,count+"|n"), (id-500,count+"|n"), (id-501,count+"|n"), (id-499,count+"|n"))
+	Array((id,count+"|c"),(id+1,count+"|n|"+id), (id-1,count+"|n|"+id), (id-500,count+"|n|"+id), (id-501,count+"|n|"+id), (id-499,count+"|n|"+id))
     }
 
+//find neighbors of cell in leftmost col
     def findLeft(id:Int, count:Float): Array[(Int,String)] = {
 	//println("left")
-	Array((id,count+"|c"), (id+1,count+"|n"), (id+500,count+"|n"), (id-500,count+"|n"), (id+501,count+"|n"), (id-499,count+"|n"))
+	Array((id,count+"|c"), (id+1,count+"|n|"+id), (id+500,count+"|n|"+id), (id-500,count+"|n|"+id), (id+501,count+"|n|"+id), (id-499,count+"|n|"+id))
     }
 
+//find neighbors of cell in rightmost col
     def findRight(id:Int, count:Float): Array[(Int,String)] = {
 	//println("right")
-	Array((id,count+"|c"), (id-1,count+"|n"), (id+500,count+"|n"), (id-500,count+"|n"), (id+499,count+"|n"), (id-501,count+"|n"))
+	Array((id,count+"|c"), (id-1,count+"|n|"+id), (id+500,count+"|n|"+id), (id-500,count+"|n|"+id), (id+499,count+"|n|"+id), (id-501,count+"|n|"+id))
     }
 
+//map cell to neighboring cells 
+//for each output neighbor id, tag, and count of points in this cell
+//output: list of tuples (id, "count|tag")
 def findNeighbors(id:Int, count:Float): Array[(Int,String)] = {
 	var row = id/500
 	var col = id % 500
@@ -48,15 +56,18 @@ def findNeighbors(id:Int, count:Float): Array[(Int,String)] = {
 	//handle edge cases
 	var corners = Array(1, 500, 249501,250000)
 	if( corners.contains(id)) findCorner(id, count)
+	else if (col == 0) findRight(id,count)
+	else if (col == 1) findLeft(id,count)
 	else if(row == 0) findTop(id, count)
 	else if(row == 499) findBottom(id, count)
-	else if (col == 0) findRight(id,count)
-        else if (col == 1) findLeft(id,count)
-        
+	
 	//regular cell
-	else Array((id,count+"|c"),(id+1,count+"|n"), (id-1,count+"|n"), (id+500,count+"|n"), (id-500,count+"|n"), (id+501,count+"|n"), (id-501,count+"|n"), (id+499,count+"|n"), (id-499,count+"|n"))
+	else Array((id,count+"|c"),(id+1,count+"|n|"+id), (id-1,count+"|n|"+id), (id+500,count+"|n|"+id), (id-500,count+"|n|"+id), (id+501,count+"|n|"+id), (id-501,count+"|n|"+id), (id+499,count+"|n|"+id), (id-499,count+"|n|"+id))
     }
 
+//Function to find the density of one cell
+//arg tags should be result of findNeighbors function
+//returns density floating point value
     def compDensity(tags:Iterable[String]): Float = {
 	var d = 0f
 	var n = 0f
@@ -74,65 +85,71 @@ def findNeighbors(id:Int, count:Float): Array[(Int,String)] = {
 	else return n/d
     }
 
-     //def listDensity(id:Int, count:Float): Array[(Int,String)] = {println(info)}
+//Function to find the relative density of neighbors
+//arg tags should be result of findNeighbors function
+//returns String of neighbor ids and density "id_1|d_1, id_2|d_2, ... id_n|d_n"
+     def listDensity(tags:Iterable[String]): String = { 
+
+	//first get this cell's density
+	var c = ""
+	var n = ""
+	
+	//then get the density of neighbors
+        for (x <- tags){
+            val cell = x.split('|')
+
+	    if(cell.apply(1) == "n"){ //neighbor
+		n += cell.apply(2)
+		n += "|"
+		n += cell.apply(0)
+		n += ","
+            }
+	    else{
+	   	c = cell.apply(0) + ","
+	    }
+	
+	}	
+	return c+n.dropRight(1)
+    }
 }
 
-
+// end of helper functions
 //////////////////////////////////////////////////////////////////////////////////////////
+
 
 //read point data
 val points = sc.textFile("/home/mqp/BigData/Project3/Problem2/points.csv")
 
+//part1
 
 //count the number of points in each cell
 
 //mapper outputs one record for each point in cell
 //reducer sums number of points in each cell
-//output: cell_id, count
+//output: (cell_id, count)
 
 val cellpointcountRDD = points.map(point => (Functions.findCell(point.split(",").map(_.toInt)), 1)).reduceByKey(_ + _)
 
 //map each cell to partition with itself and neighbors
-//reduce -  compute density for each cell
-//output: cell_id, density
+//reduce - compute density for each cell
+//output: (cell_id, density)
 
 val densityRDD = cellpointcountRDD.flatMap{ case (id, count) => Functions.findNeighbors(id, count) }.groupByKey().map{ case (id, cells) => (id, Functions.compDensity(cells))}
 
-densityRDD.take(5).foreach(println)
-
 
 //list first 50 based on the value of relative density index
-val RDI_top50 = densityRDD.takeOrdered(50)(Ordering[Float].reverse.on(x=>x._2))
+densityRDD.takeOrdered(50)(Ordering[Float].reverse.on(x=>x._2)).foreach(println)
 
+//part 2
 
-//show top 50 cell and RDI
-RDI_top50.foreach(println) 
+//map the density of each cell to itself and its neighbors 
+//then reduce to get list of ids and densities
+//output: (id, n1_id|n1_density, n2_id|2_density, ... , n2_id|2_density)
 
+val neighborDensityRDD = densityRDD.flatMap{ case (id, count) => Functions.findNeighbors(id, count) }.groupByKey().map{ case (id, cells) => (id, Functions.listDensity(cells))}
 
-//need to get top 50 cell's neighbor cell number  and its RDI
-//first join the RDI_top50 to RNC to get their neibor cell
-// remap it into a RNC_reverse1  (neighbor cell, cell)
-//RelativeDensityIndex contain all cell number and corresponding RDI,  we need to join  RNC_reverse1,  neighbor cell number, so we get (neighbor cell,  (RDI, cell)), once we get result, we remap the position and then got top 50 of them
-
-//first convert RDI_top50 to RDD
-
-val RDI_top50_RDD= sc.parallelize(RDI_top50.toSeq,1)
-
-//val top50neigborcell = RDI_top50_RDD.join(RNC)
-
-// joint result (Int, (Float, Int)) = (111635,(1.7045455,111134)), need to pick neigbor cell, rec._2._2 and current cell,  rec._1
-//val RNC_reverse1 = top50neigborcell.map(rec=>(rec._2._2, rec._1))
-
-// RelativeDensityIndex  join with RNC_reverse1
-//val RDI_top50_neighborRDI = RelativeDensityIndex.join(RNC_reverse1)
-
-//result structure res12: (Int, (Float, Int)) = (177102,(1.1702127,177103)), neibor cell, RDI, and cell, I need to remap and show all
-
-//val top50cell_neighborcell_RDI= RDI_top50_neighborRDI.map(rec=>(rec._2._2,rec._1, rec._2._1 ))
-
-//show all sort by cell numer 
-
-//top50cell_neighborcell_RDI.sortBy(_._1).collect().foreach(println)
+//list first 50 based on the value of relative density index
+neighborDensityRDD.takeOrdered(50)(Ordering[Float].reverse.on(x=>x._2.split(",").apply(0).toFloat)).foreach(println)
 
 
 
